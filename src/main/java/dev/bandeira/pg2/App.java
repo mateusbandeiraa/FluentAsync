8950f05f6a7ebfc067a3505449b0fb63201162bf
Mateus Bandeira <mateus@bandeira.dev>
2022-09-27 08:23:39 -0300

    initial commit

---
+++ src/main/java/dev/bandeira/pg2/App.java
@@ -0,0 +1,71 @@
+package dev.bandeira.pg2;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.function.Supplier;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import kong.unirest.Unirest;
+
+public class App {
+	public static void main(String[] args) {
+		final String WIKI = "https://pt.wikipedia.org/wiki/";
+
+		Unirest.config().defaultBaseUrl(WIKI);
+
+		final String verbeteInicial = "Pirelli";
+		final String verbeteFinal = "Ayrton_Senna";
+
+		final Queue<String> fila = new LinkedList<>();
+		final Set<String> verbetesVisitados = new HashSet<>();
+		fila.offer(verbeteInicial);
+
+		int iteracoes = 1;
+		while (!fila.isEmpty()) {
+			String verbete = fila.poll();
+
+			if (verbetesVisitados.contains(verbete)) {
+				continue;
+			} else if (verbete.equalsIgnoreCase(verbeteFinal)) {
+				System.out.println("Encontrei o verbete final em %d iterações.".formatted(iteracoes));
+				break;
+			}
+
+			verbetesVisitados.add(verbete);
+
+			new TarefaAssincrona<>(getVerbete(verbete)) //
+					.ramificar(App::extrairVerbetes) //
+					.filtrar(v -> !verbetesVisitados.contains(v)) //
+					.consumir(fila::offer) //
+					.unificar() //
+					.aguardar(); //
+
+			if(iteracoes == 1000) {
+				break;
+			}
+			iteracoes++;
+		}
+		System.out.println(verbetesVisitados);
+	}
+
+	public static Supplier<String> getVerbete(String verbete) {
+		System.out.println(verbete);
+		return () -> Unirest.get(verbete).asString().getBody();
+	}
+
+	private static List<String> extrairVerbetes(String paginaWiki) {
+		final Pattern WIKI_REGEX = Pattern.compile("\\/wiki\\/(\\w+?)\"");
+
+		Matcher matcher = WIKI_REGEX.matcher(paginaWiki);
+		HashSet<String> links = new HashSet<>();
+		while (matcher.find()) {
+			links.add(matcher.group(1));
+		}
+		return new ArrayList<>(links);
+	}
+}
---
+++ src/main/java/dev/bandeira/pg2/TarefaAssincrona.java
@@ -0,0 +1,40 @@
+package dev.bandeira.pg2;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class TarefaAssincrona<T> {
+	protected final CompletableFuture<T> tarefa;
+
+	private TarefaAssincrona(CompletableFuture<T> tarefa) {
+		this.tarefa = tarefa;
+	}
+
+	public TarefaAssincrona(Supplier<T> tarefa) {
+		this(CompletableFuture.supplyAsync(tarefa));
+	}
+
+	public T aguardar() {
+		return tarefa.join();
+	}
+
+	public TarefaAssincrona<Void> consumir(Consumer<T> consumidora) {
+		return new TarefaAssincrona<>(tarefa.thenAcceptAsync(consumidora));
+	}
+
+	public <U> TarefaAssincrona<U> transformar(Function<T, U> transformadora) {
+		return new TarefaAssincrona<>(tarefa.thenApplyAsync(transformadora));
+	}
+
+	public <U> TarefaRamificada<U> ramificar(Function<T, List<U>> funcaoRamificadora) {
+		return new TarefaRamificada<>(this, funcaoRamificadora);
+	}
+
+	public TarefaAssincrona<T> casoErro(Function<Throwable, ? extends T> casoErro) {
+		return new TarefaAssincrona<>(tarefa.exceptionally(casoErro));
+	}
+
+}
